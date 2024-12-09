6c577d17c7afa091d0b378a2f5e02b4386377ebc
piyan1212 <piyanstefani@gmail.com>
2024-12-09 18:42:21 +0700

    Initialize

---
+++ src/main/java/com/tagihan/tabungan/tabunganinfo/TabunganInfoApplication.java
@@ -0,0 +1,13 @@
+package com.tagihan.tabungan.tabunganinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabunganInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabunganInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tagihan/tabungan/tabunganinfo/TabunganInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagihan.tabungan.tabunganinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabunganInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
